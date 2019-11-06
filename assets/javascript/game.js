c41acff9a00a295dc0a406598656e66f6b26f09f
Max Power <dbroncosfan@gmail.com>
2019-11-05 22:10:57 -0700

    game.js update, gems now add together

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,36 +24,33 @@ var sapphireRandom = Math.floor(Math.random() * 12) + 1;
 
 //Each click on each gem adds its random value to userscore
 var startScore = 0;
-var userScore;
-var scoreHolder = "";
+var userScore = parseInt(0);
 
 $("#userScore").text(startScore);
 
-$(".img1").on("click", function(){
-    $("#img1").val(diamondRandom);
-    $("#userScore").text(scoreHolder + diamondRandom);
-    
-    //userScore = Number(diamondRandom) + Number($(this).val());
-    //$("#userScore").text(userScore);
-});
-
-$(".img2").on("click", function(){
-    userScore = Number(emeraldRandom) + Number($(this).val());
+$("#diamond").on("click", function(){
+    userScore += diamondRandom
     $("#userScore").text(userScore);
 });
 
-$(".img3").on("click", function(){
-    userScore = Number(rubyRandom) + Number($(this).val());
+$("#emerald").on("click", function(){
+    userScore += emeraldRandom
     $("#userScore").text(userScore);
 });
 
-$(".img4").on("click", function(){
-    userScore = Number(sapphireRandom) + Number($(this).val());
+$("#ruby").on("click", function(){
+    userScore += rubyRandom
     $("#userScore").text(userScore);
 });
 
+$("#sapphire").on("click", function(){
+    userScore += sapphireRandom
+    $("#userScore").text(userScore);
+});
 
 //Evaluate userscore and random number, if userscore > random number, you lose
+
+
 //If userscore and random number ===, you win
 //If userscore < random number, keep playing
 
