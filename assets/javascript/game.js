398d1d900be67eb800666359d4d01b9ea69ab4d0
Max Power <dbroncosfan@gmail.com>
2019-11-09 14:30:03 -0700

    game.js working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,56 @@
 $(document).ready(function() {
 
-//Global variables
-var randNum = []
-
-//Generate a random number between 19 and 120
+var startScore = 0;
+var userScore = 0;
+var winCounter = 0;
+var lossCounter = 0;
 var randNum = Math.floor(Math.random() * 102) + 19;
-console.log(randNum);
-
-//Write random number to page
-$("#randomNumber").html(randNum);
-
-//Generate 4 random numbers between 1 and 12
-//Assign 4 random generated numbers to individual gems
 var diamondRandom = Math.floor(Math.random() * 12) + 1;
 var emeraldRandom = Math.floor(Math.random() * 12) + 1;
 var rubyRandom = Math.floor(Math.random() * 12) + 1;
 var sapphireRandom = Math.floor(Math.random() * 12) + 1;
 
+
+//Generate a random number between 19 and 120
+//Generate 4 random numbers between 1 and 12
+//Assign 4 random generated numbers to individual gems
 //Each click on each gem adds its random value to userscore
 //Evaluate userscore and random number, if userscore > random number, you lose
 //If userscore and random number ===, you win
 //If userscore < random number, keep playing
-var startScore = 0;
-var userScore = parseInt(0);
-var winCounter = 0;
-var lossCounter = 0;
 
+
+
+function resetGame(){
+    startScore = 0;
+    userScore = 0;
+    diamondRandom = 0;
+    emeraldRandom = 0;
+    rubyRandom = 0;
+    sapphireRandom = 0;
+    randNum = Math.floor(Math.random() * 102) + 19;
+    $("#randomNumber").text(randNum);    
+    $("#userScore").text(startScore);
+
+    startGame();
+}
+
+function startGame() {
+
+    randNum = Math.floor(Math.random() * 102) + 19;
+    diamondRandom = Math.floor(Math.random() * 12) + 1;
+    emeraldRandom = Math.floor(Math.random() * 12) + 1;
+    rubyRandom = Math.floor(Math.random() * 12) + 1;
+    sapphireRandom = Math.floor(Math.random() * 12) + 1;
+    console.log(diamondRandom);
+    console.log(emeraldRandom);
+    console.log(rubyRandom);
+    console.log(sapphireRandom);
+
+//Write random number to page
+$("#randomNumber").text(randNum);    
 $("#userScore").text(startScore);
+}
 
 $("#diamond").on("click", function(){
     userScore += diamondRandom
@@ -35,11 +59,13 @@ $("#diamond").on("click", function(){
         winCounter++;
         document.getElementById("wins").innerHTML = winCounter;
         alert("Winner Winner");
+        resetGame();
     }
     else if (userScore > randNum) {
         lossCounter++;
         document.getElementById("losses").innerHTML = lossCounter;
         alert("You Lose!");
+        resetGame();
     }
 });
 
@@ -50,11 +76,13 @@ $("#emerald").on("click", function(){
         winCounter++;
         document.getElementById("wins").innerHTML = winCounter;
         alert("Winner Winner");
+        resetGame();
     }
     else if (userScore > randNum) {
         lossCounter++;
         document.getElementById("losses").innerHTML = lossCounter;
         alert("You Lose!");
+        resetGame();
     }
 });
 
@@ -65,11 +93,13 @@ $("#ruby").on("click", function(){
         winCounter++;
         document.getElementById("wins").innerHTML = winCounter;
         alert("Winner Winner");
+        resetGame();
     }
     else if (userScore > randNum) {
         lossCounter++;
         document.getElementById("losses").innerHTML = lossCounter;
         alert("You Lose!");
+        resetGame();
     }
 });
 
@@ -80,26 +110,16 @@ $("#sapphire").on("click", function(){
         winCounter++;
         document.getElementById("wins").innerHTML = winCounter;
         alert("Winner Winner");
+        resetGame();
     }
     else if (userScore > randNum) {
         lossCounter++;
         document.getElementById("losses").innerHTML = lossCounter;
         alert("You Lose!");
+        resetGame();
     }
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+startGame();
 })
